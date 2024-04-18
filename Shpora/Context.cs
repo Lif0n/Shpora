ca4f10286cb435a6ba461262f0bbe49f96025ace
Lif0n <t4sty.c4ke@gmail.com>
2024-04-19 03:01:02 +0400

    Добавьте файлы проекта.

---
+++ Shpora/Context.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Shpora.Models;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+
+namespace Shpora
+{
+    public class Context : DbContext
+    {
+        private string _connectionString = "Data Source=DESKTOP-HIITB3O; initial Catalog=test;Integrated Security=True;TrustServerCertificate=True";
+
+        public DbSet<User> test { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+        public Context()
+        {
+
+        }
+    }
+}
---
+++ Shpora/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Shpora.Models;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Shpora
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Context _db;
+        public MainWindow()
+        {
+            _db = new Context();
+            InitializeComponent();
+            UserList.ItemsSource = _db.test.ToList();
+        }
+
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var selecteduser = UserList.SelectedItem as User;
+            MessageBox.Show(selecteduser.Name);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("ButtonClick");
+        }
+    }
+}
